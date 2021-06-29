1c4b373779e8fcabe4652cb0e65734bc4c4bc4c1
Ashwin M.A <85541702+AShuuu-Technoid@users.noreply.github.com>
2021-06-29 11:32:09 +0530

    fix

--- setup.sh
+++ setup.sh
@@ -2011,7 +2011,7 @@ ins(){
             exit
     else
             # apt-get install -y zenity >/dev/null
-            ListType=$(zenity --window-icon ".res/rage.png" --width=400 --height=490 --checklist --list \
+            ListType=$(zenity --window-icon ".res/rage.png" --width=400 --height=500 --checklist --list \
                 --title='Ubuntu Software Installation'\
                 --text="<b>Select Software to install :</b>\n <span foreground='red' font='10'>⚠️ NOTE : Don't select Domain-join in multi selection. ⚠️ </span>"\
                 --column="Select" --column="Software List" \
